2ef5e3cdfc00dcde409a1c2c3bd1cdc8f3438f04
Chaitanya Gawali <75407657+iamchaitanya7@users.noreply.github.com>
2023-08-14 01:36:09 +0530

    Python Variable Creation

---
+++ Variable.py
@@ -0,0 +1,19 @@
+No = 1 #stores integer values
+print(type(No))
+print(No)
+
+No = 11.1 #stores Float values
+print(type(No))
+print(No)
+
+No = 3+4j #stores Complex Number
+print(type(No))
+print(No)
+
+N = "Hello" #stores String values
+print(type(No))
+print(No)
+
+No = True #stores boolean values
+print(type(No))
+print(No)
