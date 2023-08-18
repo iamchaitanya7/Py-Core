d89d3141efdde38f268812514a636f5a9739e0f1
Chaitanya Gawali <75407657+iamchaitanya7@users.noreply.github.com>
2023-08-18 23:00:32 +0530

    Add files via upload

---
+++ ListLooping.py
@@ -0,0 +1,14 @@
+arr = [11, 21, 51, 101]
+
+#for loop
+for i in range(len(arr)):
+    print(arr[i])
+
+for i in range(0, len(arr), 1):
+    print(arr[i])
+
+#while loop
+i  = 0
+while (i < len(arr)):
+    print(arr[i])
+    i = i + 1
