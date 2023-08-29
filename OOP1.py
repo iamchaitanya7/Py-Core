9fa1be0131792ca06e828154bf16566869b9ee99
Chaitanya Gawali <75407657+iamchaitanya7@users.noreply.github.com>
2023-08-29 23:57:05 +0530

    PyOOP_FC

---
+++ OOP1.py
@@ -0,0 +1,12 @@
+
+class Demo:
+    def __init__(self):       #Constructor
+        print("Inside Init")
+
+def main():
+    print("Demo of OOP")
+    obj1 = Demo()
+    obj2 = Demo()
+
+if __name__ == "__main__":
+    main()
