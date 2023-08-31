f2a83f00a3971645f28dec92f4e5a5b678e9bdf5
Chaitanya Gawali <75407657+iamchaitanya7@users.noreply.github.com>
2023-08-31 23:45:32 +0530

    Add files via upload

---
+++ Module.py
@@ -0,0 +1,19 @@
+def Addition(No1, No2):
+    ans = 0
+    ans = No1 + No2
+    return ans
+
+def Substraction(No1, No2):
+    ans = 0
+    ans = No1 - No2
+    return ans
+
+def Multiplication(No1, No2):
+    ans = 0
+    ans = No1 * No2
+    return ans
+
+def Division(No1, No2):
+    ans = 0
+    ans = No1 / No2
+    return ans
