38d167c2475b943f50f1aba3f8233e8738140555
Chaitanya Gawali <75407657+iamchaitanya7@users.noreply.github.com>
2023-09-07 23:36:04 +0530

    Add files via upload

---
+++ Decorator.py
@@ -0,0 +1,12 @@
+
+def Sub(A, B):
+    return A - B
+
+def main():
+    print("Substraction is: ", Sub(10, 7))
+    print("Substraction is: ", Sub(7, 10))
+
+
+
+if __name__ == "__main__":
+    main()
