dd9a69bcca8168f6e5939a66279dabf3b0b9feaf
Chaitanya Gawali <75407657+iamchaitanya7@users.noreply.github.com>
2023-10-12 22:58:30 +0530

    recursion fc

---
+++ recursion3.py
@@ -0,0 +1,17 @@
+
+def DisplayFor():
+    for i in range(1, 6):
+        print(i)
+        
+def DisplayWhile():
+    i = 1
+    while (i < 6):
+        print(i)
+        i += 1        
+        
+def main():
+    DisplayFor()
+    DisplayWhile()
+
+if __name__ == "__main__":
+    main()
