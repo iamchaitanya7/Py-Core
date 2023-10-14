6e6122266041d22d55fdb89a261b645a20687d96
Chaitanya Gawali <75407657+iamchaitanya7@users.noreply.github.com>
2023-10-14 23:26:52 +0530

    Exception FC

---
+++ trydemo.py
@@ -0,0 +1,18 @@
+def main():
+
+    print("Enter First No")
+    no1 = int(input())
+
+    print("Enter Second No")
+    no2 = int(input())
+
+    ans = 0
+
+    ans = no1 / no2
+
+    print("Division is: ", ans)
+
+
+    
+if __name__ == "__main__":
+    main()
