84aed253aa22767c93a99347f9a6d5659c6c79c7
Chaitanya Gawali <75407657+iamchaitanya7@users.noreply.github.com>
2023-10-14 23:27:06 +0530

    Exception FC

---
+++ trydemo1.py
@@ -0,0 +1,20 @@
+def main():
+
+    print("Enter First No")
+    no1 = int(input())
+
+    print("Enter Second No")
+    no2 = int(input())
+
+    ans = 0
+    try:
+        ans = no1 / no2
+    except ZeroDivisionError as zobj:
+        print("Divide by Zero Not Allowed", zobj)
+
+    print("Division is: ", ans)
+
+
+    
+if __name__ == "__main__":
+    main()
