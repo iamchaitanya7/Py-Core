0313fc3197087883dfc9618deaf6d6da0a9d4321
Chaitanya Gawali <75407657+iamchaitanya7@users.noreply.github.com>
2023-10-15 23:51:42 +0530

    Exception FC

---
+++ trydemo6.py
@@ -0,0 +1,32 @@
+def main():
+
+    try:
+        print("Enter First No")
+        no1 = int(input())
+
+        print("Enter Second No")
+        no2 = int(input())
+
+        ans = 0
+
+        ans = no1 / no2
+
+    except ZeroDivisionError as zobj:
+        print("Divide by Zero Not Allowed", zobj)
+        return
+
+    except ValueError as vobj:
+        print("Invalid Input", vobj)
+        return
+        
+    except Exception as obj:
+        print("Exception occured: ", obj)
+        return
+
+    finally:
+        print("Thank you for using the application")
+
+    print("Division is: ", ans)
+
+if __name__ == "__main__":
+    main()
